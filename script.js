a7e74adbdb5b42f930c6bd3c2ace76c1eafe24de
Ricardo Oliveira <ricardooliveiracwbsi@gmail.com>
2025-06-19 12:48:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function mensagemdemotivacao() {
+    document.getElementById("mensagem").innerText = 
+      "Seja bem vindo(a)! Você é capaz de aprender qualquer coisa. Continue praticando!";}
+  
